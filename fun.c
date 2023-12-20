5a47e88952a94b2f7f7eddf723240437830f07b2
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2023-12-20 16:46:25 +0530

    Add files via upload

---
+++ fun.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void myFunction(){
+    printf("hello Tis is first function\n");
+     myFunction1();
+}
+void myFunction1(){
+    printf(" Hello This is Second Function");
+}
+int main(){
+   myFunction();
+     myFunction1();
+}
+
+
+
+
