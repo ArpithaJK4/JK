784985838c1dbd2ad47d4d67e8dea4cab440be3f
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2023-12-27 19:54:16 +0530

    Add files via upload

---
+++ Happy.java
@@ -0,0 +1,31 @@
+
+
+public class Happy {
+	
+	
+	
+	public static void main(String[]args){
+  int no=80;
+  while(no>9){
+	  no=num(no);
+  }
+  if(no==7||no==1){
+	  System.out.println("happy number");
+  }else{
+	  System.out.println("not a happy number");
+  }
+
+}
+
+	private static int num(int no) {
+		 int sum=0;
+		
+		 while(no!=0){
+			 int rem=no%10;
+			 sum=sum+rem*rem;
+			 no=no/10;
+			 
+		 }
+			return sum;
+	}
+}
